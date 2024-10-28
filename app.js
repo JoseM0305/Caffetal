af8d5d505fc4fe36a02fa58c49c86f47bfefd817
José Mario <josehiginio03@gmail.com>
2024-10-27 21:20:04 -0500

    Completando MVC, login y register

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //Invocamos a express
+const app = express(); //Nos va a permitir usar todos los métodos de la libreria
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +8,6 @@ app.use(express.json());
 
 app.use('/', require('./router'));
 
-app.listen(5000, ()=>{
+app.listen(5000, ()=>{ //Nos permite ejecutarlo en el puerto 5000
     console.log('Servidor corriendo en http://localhost:5000')
 });
